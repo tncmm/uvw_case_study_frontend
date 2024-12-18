1e12f3e464134d591518811922a470cef2f20f0a
Mustafa Tunç <tncmmustafa@gmail.com>
2024-12-18 22:35:00 +0300

    eslint bug fixed

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @next/next/no-html-link-for-pages */
 
 import { loginUser } from '@/redux/slices/authSlice';
 import { useAppDispatch } from '../../redux/hooks/hooks';
